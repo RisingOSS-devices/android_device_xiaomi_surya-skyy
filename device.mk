607eed2b3a0f0f76ba9f202e44044cee2ed4d338
HinohArata <mmgcntk@gmail.com>
2024-04-24 22:58:27 +0700

    surya: Include MiuiCamera if exist
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- device.mk
+++ device.mk
@@ -110,6 +110,8 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Camera
+$(call inherit-product-if-exists, vendor/xiaomi/surya-miuicamera/products/miuicamera.mk)
+
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
