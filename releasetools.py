44aeaf169b08eb59758f9f4bbc92ef97c57417ef
HinohArata <mmgcntk@gmail.com>
2024-04-25 17:40:23 +0700

    surya: cleanup
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 231d2baa (surya: Include firmware Images)
 #
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (C) 2019 The Mokee Open Source Project
 # Copyright (C) 2020-2021 The LineageOS Open Source Project
-<<<<<<< HEAD
-=======
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,20 +17,6 @@
 #
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
->>>>>>> 231d2baa (surya: Include firmware Images)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 import common
 import re
