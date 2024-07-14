9a0e63860f2441f24ced5ce6d0b7fd3df688ee0f
HinohArata <mmgcntk@gmail.com>
2024-07-14 17:41:19 +0000

    surya: Compile kernel using latest clang from google
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,9 +109,10 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 TARGET_KERNEL_CONFIG := surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
-KERNEL_SUPPORTS_LLVM_TOOLS := true
-KERNEL_LD := LD=ld.lld
-TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 LLVM_IAS=1
++TARGET_KERNEL_CLANG_COMPILE := true
+KERNEL_CC := CC=clang
+TARGET_KERNEL_CLANG_VERSION := clang-r522817
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r522817
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
